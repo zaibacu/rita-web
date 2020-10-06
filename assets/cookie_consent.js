e21af36c766c407973aa308b0cdabc7824530fb5
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2020-10-06 10:49:07 +0300

    Simple, working cookie consent

---
+++ assets/cookie_consent.js
@@ -0,0 +1,18 @@
+/*
+ * Borrowed from https://codeshack.io/eu-cookie-consent-popup-javascript/
+ */
+(function() {
+	if (!localStorage.getItem('cookieconsent')) {
+		document.body.innerHTML += '\
+		<div class="cookieconsent" style="position:fixed;padding:20px;left:0;bottom:0;background-color:#000;color:#FFF;text-align:center;width:100%;z-index:99999;">\
+			This site uses cookies. By continuing to use this website, you agree to their use. \
+			<a href="#" style="color:#CCCCCC;">I Understand</a>\
+		</div>\
+		';
+		document.querySelector('.cookieconsent a').onclick = function(e) {
+			e.preventDefault();
+			document.querySelector('.cookieconsent').style.display = 'none';
+			localStorage.setItem('cookieconsent', true);
+		};
+	}
+})();
